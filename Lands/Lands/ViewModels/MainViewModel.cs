9fcd9b457690fa5741bce16d9200039083761a71
DRFLASH1312 <detal446@gmail.com>
2019-02-05 22:01:08 -0600

    Login Page Complete

---
+++ Lands/Lands/ViewModels/MainViewModel.cs
@@ -0,0 +1,22 @@
+﻿
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Lands.ViewModels
+{
+    public class MainViewModel
+    {
+        #region ViewModels
+        public LoginViewModel Login
+        {
+            get;
+            set;
+        }
+        #endregion
+        public MainViewModel()
+        {
+            this.Login = new LoginViewModel();
+        }
+    }
+}
